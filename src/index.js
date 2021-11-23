528f46c6dc455745e405f8f5181dcc7492fae729
go367it <satyamkumar3001@gmail.com>
2021-11-23 19:32:30 +0530

    included google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-213562295-1')
 
 ReactDOM.render(
   <BrowserRouter>
